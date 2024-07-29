b56e7476c8ac272155becef44bdc74b8d50f479f
CoroSempi <corosempi@gmail.com>
2024-07-30 00:01:55 +0300

    first commit

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,9 @@
-import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import SegmentRoundedIcon from "@mui/icons-material/SegmentRounded";
-import MailIcon from "@mui/icons-material/Mail";
 
 export default function LList() {
   const [state, setState] = React.useState(false);
